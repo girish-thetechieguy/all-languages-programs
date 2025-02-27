12b0636fc0cac78ac0cb09a58e1469e189701fbc
girish.balekundri <girish.thetechieguy@gmail.com>
2025-02-27 09:42:10 +0800

    Java 1.0 Features 1: Basic structure of the program

---
+++ 01-Java-1.0-Featutes/src/_01_java_structure.java
@@ -0,0 +1,6 @@
+// Simple Hello World program
+public interface _01_java_structure {
+    public static void main(String[] args) {
+        System.out.println("Hi all! Welcome to My java world!");
+    }
+}
