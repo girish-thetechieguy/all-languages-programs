6148ed5a4b37c52eeb50c1e42361985fb888b887
girish.balekundri <girish.thetechieguy@gmail.com>
2025-03-17 23:14:45 +0800

    Removed target folder

--- 02_Junit/src/test/java/_01_BasicServiceTest.java
+++ 02_Junit/src/test/java/_01_BasicServiceTest.java
@@ -2,9 +2,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class _01_BasicServiceTest {
-
-
-
     @Test
     void getGradeTest(){
         _01_BasicService basicService = new _01_BasicService();
